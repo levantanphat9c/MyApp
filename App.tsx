75ccc093f8110e677c56635b14c01e23b262013a
Le Van Tan Phat <phat.le@difisoft.com>
2024-03-05 15:16:38 +0700

    add redux toolkit and saga

--- App.tsx
+++ App.tsx
@@ -1,53 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {PersistGate} from 'redux-persist/integration/react';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 import Navigation from './src/Navigation';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+import {Provider} from 'react-redux';
+import {persistor, store} from '@/ReduxSaga';
+import {View} from 'react-native';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return <Navigation />;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <View style={{flex: 1}}>
+          <Navigation />
+        </View>
+      </PersistGate>
+    </Provider>
+  );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
--- src/Containers/HomeScreen/index.tsx
+++ src/Containers/HomeScreen/index.tsx
@@ -5,10 +5,20 @@ import styles from './styles';
 import {ROUTE_NAME} from '@/Navigation/RouteName';
 import {HomeStackScreenProps} from '@/Navigation/NavigationType';
 import Config from 'react-native-config';
+import {useDispatch, useSelector} from 'react-redux';
+import {CounterActions, CounterReducerSelectors} from '@/ReduxSaga/Counter';
+import AppText from '@/Components/AppText';
 
 const HomeScreen = ({
   navigation,
 }: HomeStackScreenProps<ROUTE_NAME.HOME_SCREEN>) => {
+  const value = useSelector(CounterReducerSelectors.selectCount);
+  const dispatch = useDispatch();
+
+  const onIncrement = () => {
+    dispatch(CounterActions.incrementSaga());
+  };
+
   return (
     <View style={styles.container}>
       <TouchableOpacity
@@ -17,8 +27,15 @@ const HomeScreen = ({
             userId: '123',
           });
         }}>
-        <Text>Go to Setting Screen</Text>
+        <AppText type={'BUTTON_2'} color={'red'}>
+          Go to Setting Screen
+        </AppText>
         <Text>{Config.API_URL}</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={onIncrement}>
+        <Text>Increment</Text>
+        <Text>{value}</Text>
       </TouchableOpacity>
     </View>
   );
